ed8b7c72b6cc25f7be1afe35ecccbc16fb8ee6b8
Antonio <antocecere77@gmail.com>
2019-02-09 10:42:15 +0100

    Creazione app Movie list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import VideoList from './components/movie_list';
+
+const APIKEY = '802ea4db'
+const APIURL = 'http://www.omdbapi.com'
+
+function fetchMovies(search = 'back to the future') {
+  return fetch(APIURL + '?apikey=' + APIKEY + '&s=' + search).then(res => res.json());
+}
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      totalCount: 0
+    }
+  }
+
+  componentDidMount() {
+    fetchMovies().then(res => {
+      //console.log(res);
+      this.setState({
+        movies: res.Search,
+        totalCount: res.totalCount
+      })
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+       <h1>My favorite movies</h1>
+   
+        <VideoList movies={this.state.movies}/>
+       
       </div>
     );
   }
