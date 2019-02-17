7ecd78a07a66d629d7447143a5fa83364fe2890f
Antonio Cecere <cece@mo.rds-software.com>
2019-02-17 10:50:56 +0100

    Updates

---
+++ precache-manifest.672610bb2f0c84a016958f69705a9254.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7a0a7d64085caf4b557",
+    "url": "/Movie_list/static/js/runtime~main.f7a0a7d6.js"
+  },
+  {
+    "revision": "934fc8316c4775dc08f8",
+    "url": "/Movie_list/static/js/main.934fc831.chunk.js"
+  },
+  {
+    "revision": "1015926caf23046f2ba3",
+    "url": "/Movie_list/static/js/1.1015926c.chunk.js"
+  },
+  {
+    "revision": "934fc8316c4775dc08f8",
+    "url": "/Movie_list/static/css/main.ba5d6735.chunk.css"
+  },
+  {
+    "revision": "3eca14e48a7478232a41190225ea06ac",
+    "url": "/Movie_list/index.html"
+  }
+];
